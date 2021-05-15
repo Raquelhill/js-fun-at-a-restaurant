1ffe6025426d1615534b3b04bf3be19990c237ce
Raquel Hill <raquelturing@gmail.com>
2021-05-14 18:50:37 -0600

    finished meals.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`
+};
 
-}
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type,
+  }
+};
 
+function addIngredients(extraIngredient, ingredients) {
+  if (ingredients.includes(extraIngredient)) {
+    return
+  } else {
+    return ingredients.push(extraIngredient)
+  }
+};
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+};
+
+function decreasePrice(price) {
+  var discount = (price * .10)
+  var decreasePrice = (price - discount)
+  return decreasePrice
+};
+
+function createRecipe(title, ingredients, menuItemType) {
+  return recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+};
 
 module.exports = {
+
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
